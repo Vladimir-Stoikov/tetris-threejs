04f77181d1abac97ab8d3fabccb7fd4d1bca5999
Vladimir Stoikov <vspsmoke@gmail.com>
2025-06-24 20:58:00 +0300

    feat(fieldmesh): start added animations and visual effects

--- src/components/FieldMesh.tsx
+++ src/components/FieldMesh.tsx
@@ -8,20 +8,19 @@ type FieldMeshProps = {
 export const FieldMesh = ({ field }: FieldMeshProps) => {
   const cells = useMemo(() => {
     const elements: JSX.Element[] = [];
-
     for (let y = 0; y < field.length; y++) {
+      const rowComplete = field[y].every(cell => cell !== 0);
       for (let x = 0; x < field[y].length; x++) {
         if (field[y][x] !== 0) {
           elements.push(
             <mesh position={[x, -y, 0]} key={`${x}-${y}`}>
               <boxGeometry args={[0.9, 0.9, 0.9]} />
-              <meshBasicMaterial color="gray" />
+              <meshStandardMaterial color={rowComplete ? 'white' : 'gray'} emissive={rowComplete ? 'white' : 'black'} emissiveIntensity={rowComplete ? 1 : 0} />
             </mesh>
           );
         }
       }
     }
-
     return elements;
   }, [field]);
 

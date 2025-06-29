e190933c695efcd04ff4ab89119c15e177bbd2f0
Vladimir Stoikov <vspsmoke@gmail.com>
2025-06-29 19:42:27 +0300

    fix(useameloop): update depends and type

--- src/game/core/useGameLoop.ts
+++ src/game/core/useGameLoop.ts
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
 
-export const useGameLoop = (callback: () => void ) => {
+import { useEffect, useRef } from "react";
+
+export const useGameLoop = (callback: () => void, dependencies: React.DependencyList = []) => {
+  const savedCallback = useRef<() => void>();
+
+ 
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
   useEffect(() => {
     let frameId: number;
+    let running = true;
+
     const loop = () => {
-      callback();
+      if (!running) return;
+      savedCallback.current?.();
       frameId = requestAnimationFrame(loop);
     };
+
     loop();
-    return () => cancelAnimationFrame(frameId);
-  }, [callback]);
+
+    return () => {
+      running = false;
+      if (frameId) cancelAnimationFrame(frameId);
+    };
+  }, dependencies); 
 };

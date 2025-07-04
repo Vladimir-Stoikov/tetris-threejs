f8182e8dc79f9800b8260d84239f4451a98563ce
Vladimir Stoikov <vspsmoke@gmail.com>
2025-07-04 23:01:53 +0300

    fix(usegameloop): component refactor

--- src/game/core/useGameLoop.ts
+++ src/game/core/useGameLoop.ts
@@ -1,29 +1,23 @@
-
 import { useEffect, useRef } from "react";
 
-export const useGameLoop = (callback: () => void, dependencies: React.DependencyList = []) => {
+export const useGameLoop = (callback: () => void) => {
   const savedCallback = useRef<() => void>();
 
- 
+
   useEffect(() => {
     savedCallback.current = callback;
-  }, [callback]);
+  });
 
   useEffect(() => {
     let frameId: number;
-    let running = true;
 
     const loop = () => {
-      if (!running) return;
       savedCallback.current?.();
       frameId = requestAnimationFrame(loop);
     };
 
-    loop();
+    frameId = requestAnimationFrame(loop);
 
-    return () => {
-      running = false;
-      if (frameId) cancelAnimationFrame(frameId);
-    };
-  }, dependencies); 
+    return () => cancelAnimationFrame(frameId);
+  }, []);
 };
